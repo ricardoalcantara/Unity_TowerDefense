1b1c95a8337826c4e105b44005eb2578dee597f6
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2018-07-15 00:46:52 -0300

    S2E3 Unity Navmesh Basics

---
+++ TowerDefense/Assets/Scripts/BasicMoviment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicMoviment : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent;
+
+    [SerializeField]
+    private GameObject _targetGameobject;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.destination = _targetGameobject.transform.position;
+    }
+
+    void Update()
+    {
+
+    }
+}
