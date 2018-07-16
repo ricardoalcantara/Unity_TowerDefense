2c6c46f762d29553542670123779d7f8fcb23180
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2018-07-16 12:05:07 -0300

    S2E12 Tower Defense Turret Targeting

--- TowerDefense/Assets/Scripts/BasicTargeting.cs
+++ TowerDefense/Assets/Scripts/BasicTargeting.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class BasicTargeting : MonoBehaviour
 {
+    public bool showGizmos = false;
+    public float range = 8f;
+    public float rotationSpeed = 100f;
     public Transform target;
 
     void Update()
     {
-        transform.LookAt(target);
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        Quaternion desiredRotation;
+
+        if (distance <= range)
+        {
+            Vector3 direction = transform.position - target.position;
+            desiredRotation = Quaternion.LookRotation(direction);
+        }
+        else
+        {
+            desiredRotation = Quaternion.LookRotation(Vector3.zero);
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (showGizmos)
+        {
+            // Draw a yellow sphere at the transform's position
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
     }
 }
